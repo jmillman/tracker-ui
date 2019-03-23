62a048869ba0c5caf75eae9018886c4a2ab7bf8b
Jared <jared@pcwaiter.com>
2019-03-23 14:36:59 -0400

    implemented item types

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,34 +1,125 @@
 import React, { useContext, useState } from 'react';
 import GlobalContext from '../store/GlobalContext';
+import { debug } from '../utils';
+
+import { itemTypes, ItemDataTypes } from '../templates/itemTypes';
 
 import {
-    Container,
     Button,
-    Header
+    Container,
+    Form,
+    Input,
+    Segment,
+    Select,
+    Label,
   } from 'semantic-ui-react';
+
+const useTypingSelectionBox = true;
 
 function AddItem() {
     const [ , , api] = useContext(GlobalContext);
     const [valueToAdd, setValueToAdd] = useState('');
+    const [selectedItemType, setSelectedItemType] = useState('');
 
     function handleClickCreate() {
-        api.addItemFromApp(valueToAdd, 'GYM');
+        api.addItemFromApp(valueToAdd, selectedItemType);
+    }
+
+    function itemFormChooser() {
+        const itemTypeSelected = itemTypes.find(({name}) => name == selectedItemType);
+        const returnArray = [];
+        if(itemTypeSelected) {
+            switch(itemTypeSelected.itemDataType) {
+                case ItemDataTypes.NUMBER:
+                    returnArray.push (
+                        <Form.Field key={'EventType'}>
+                            <input
+                                placeholder={itemTypeSelected.dataTypeName}
+                                value={valueToAdd}
+                                onChange={(e) => setValueToAdd(e.target.value)}
+                            />
+                        </Form.Field>
+                    );
+                    break;
+                case ItemDataTypes.EVENT_HAPPENED:
+                default:
+                        break;
+            }
+
+            returnArray.push(
+                <Form.Field key={'AddButton'}>
+                    <Button positive onClick={handleClickCreate}>Add</Button>
+                </Form.Field>
+            );
+        }
+        else if (selectedItemType !== '') {
+            returnArray.push(
+                <Label color='red' key='addNotFound'>
+                    Item Type Not Found
+                </Label>
+            );
+        }
+        return returnArray;
+    }
+
+    function getSelectTypeChooser() {
+        if( useTypingSelectionBox ) {
+            return (
+                <Form.Field>
+                    <Input
+                        list='itemTypesList'
+                        placeholder='Select a type to track...'
+                        onChange={(e)=>{setSelectedItemType(e.target.value)}}
+                    /> 
+                    <datalist id='itemTypesList'>
+                    {itemTypes.map((itemType) => {
+                        return (
+                            <option
+                                value={itemType.name}
+                                key={itemType.name}
+                            />
+                        );
+                    })}
+                    </datalist>
+                </Form.Field> 
+            );
+        }
+
+        const pulldownOptions = itemTypes.map((itemType) => {
+            return {
+                key: itemType.name,
+                text: itemType.name,
+                value: itemType.name,
+            };
+        });
+
+        return (
+            <Form.Field>
+                <Select
+                    placeholder='Select a type to track...'
+                    options={pulldownOptions}
+                    onChange={(e, data)=>{
+                        debug({e, data});
+                        setSelectedItemType(data.value)
+                    }}
+                />
+            </Form.Field>
+        );
+
+
     }
 
     return (
         <>
-            <Header as='h3' textAlign='center' content='Add Item' />
             <Container text>
-                <input
-                    type='text'
-                    value={valueToAdd}
-                    onChange={(e) => setValueToAdd(e.target.value)}
-                />
-                <Button positive
-                    onClick={handleClickCreate}
-                >
-                    Add
-                </Button>
+                <Segment.Group>
+                    <Segment>
+                        <Form>
+                            {getSelectTypeChooser()}
+                            {itemFormChooser()}
+                    </Form>
+                    </Segment>
+                </Segment.Group>
             </Container>
         </>
     );
--- src/components/ItemCards.js
+++ src/components/ItemCards.js
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react';
 import GlobalContext from '../store/GlobalContext';
+import { debug } from '../utils';
 
 import {
     Container,
     Button,
     Segment,
-    Header,
   } from 'semantic-ui-react';
 
 function ItemCard() {
@@ -14,21 +14,14 @@ function ItemCard() {
     function handleClickDelete(id) {
         api.deleteItemFromApp(id);
     }
-
     return (
         <>
-            <Header as='h3' textAlign='center' content='Container' />
             <Container text>
                 <Segment.Group>
-                    <Segment>
-                        <Button onClick={api.fetchItemsFromApp}>
-                            Refresh
-                        </Button>
-                    </Segment>
                     {state.data && state.data.map(item => {
                         return (
                             <Segment key={item.id}>
-                                {item.value}
+                                {item.value} {item.type} {item.date}
                                 <Button negative
                                     key={item.i}
                                     onClick={() => handleClickDelete(item.id)}
